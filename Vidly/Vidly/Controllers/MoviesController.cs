a53835947967264a22e59ad1d66a7044fa9c184a
Javier <javidublin01@hotmail.com>
2017-07-03 20:55:13 +0100

    Routing
    Manual and Attribute Routing

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -9,11 +9,47 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        // GET: Movies / Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Pulp Fiction" };
             return View(movie);
+            //return new ViewResult();
+            //return Content("Hello world!"); 
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index","Home", new {page = 1 , SortBy = "name"});
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}",pageIndex,sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "+" + month);
+        }
+
+        [Route("movies/released2/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate2(int year, int month)
+        {
+            return Content(year + "+" + month);
         }
     }
 }
